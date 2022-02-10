997a62634190805fd1bd3f0d35e2080b2424d3ce
M0rqpey <a@g.com>
2022-02-10 23:18:46 +0200

    test multiprocessing

--- 1.py
+++ 1.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import multiprocessing as mp
 import numpy as np
 import time
 import polars as pl
@@ -6,10 +7,12 @@ from some_funcs import get_all_ind
 
 
 list_all_ind = get_all_ind(16)
-#"""
+#""" # numpy
+rows = 40
+cols = 20
 some_df = pd.DataFrame(
-    data=np.random.randint(0,3+1, size=(20,20)),
-    columns=[f"col{i}" for i in range(20)]
+    data=np.random.randint(0,3+1, size=(rows,cols)),
+    columns=[f"col{i}" for i in range(cols)]
 )
 start = time.time()
 c = 1
@@ -19,10 +22,10 @@ for i in range(0,some_df.shape[0] -10, 5):
     c +=1
     for idx in list_all_ind:
         # print(some_df.iloc[i:i+10, idx])
-        # print(np_data[i:i+10, idx])
-        print(pl.DataFrame(np_data[i:i+10, idx]))
+        print(np_data[i:i+10, idx])
+        # print(pl.DataFrame(np_data[i:i+10, idx]))
 print(time.time() - start)
-"""
+""" # polars
 some_df = pl.DataFrame(
     data=np.random.randint(0,3+1, size=(20,20)),
     columns=[f"col{i}" for i in range(20)]
--- some_t.py
+++ some_t.py
@@ -1,25 +1,68 @@
 import pandas as pd
+import math
+import multiprocessing as mp
 import numpy as np
 import time
 import polars as pl
-# from some_funcs import get_all_ind
+from some_funcs import get_all_ind
 
 
-# list_all_ind = get_all_ind(16)
-c = 0
-for _ in range(500):
+list_all_ind = get_all_ind(16)
+BUNCH = 15
+MAX_PROCESSES = 11
+
+def f(data, name_pr, d=None):
+    for i in range(0, data.shape[0], 5):
+        for idx in list_all_ind:
+            rez = data[i:i+10, idx]
+            un, count = np.unique(rez, axis=0, return_counts=True)
+            rez = un[count>1]
+            if rez.shape[0] > 0:
+                print(rez)
+
+
+def main():
+    rows = 500
+    cols = 20
     some_df = pl.DataFrame(
-        data=np.random.randint(0,3+1, size=(20,8)),
-        columns=[f"col{i}" for i in range(8)]
+        data=np.random.randint(0,3+1, size=(rows, cols)),
+        columns=[f"col{i}" for i in range(cols)]
     )
-    rez = some_df[some_df.is_duplicated()].distinct(keep="first")
-    if rez.shape[0] > 0:
-        print(some_df)
-        mask = some_df.is_duplicated()
-        print(some_df[mask])
-        print(rez)
-        for i in rez.rows():
+    some_df = some_df.with_row_count("id")
+    print(some_df)
+    np_data = some_df.to_numpy()
+    bunch_x15 = math.ceil(np_data.shape[0] / BUNCH / MAX_PROCESSES)
+    count_process = math.ceil(np_data.shape[0] / BUNCH / bunch_x15)
 
-            print(list(i))
-        c += 1
-print(c)
+    print(f"count banch per process = {bunch_x15}")
+    print(f"count_process = {count_process}")
+
+    all_pr = []
+    q = mp.Queue()
+    #mp.set_start_method("spawn")
+    with mp.Manager() as manager:
+        d = manager.dict()
+        c = 1
+        for i in range(0, np_data.shape[0], bunch_x15 * BUNCH):
+            # print(i,i+bunch_x15 * BUNCH)
+            # print(np_data[i:i+bunch_x15 * BUNCH].shape)
+            name = f"pr-{c}"
+            pr = mp.Process(
+                name=name,
+                target=f,
+                args=(np_data[i:(i + bunch_x15 * BUNCH)],name, d)
+            )
+            c += 1
+            pr.start()
+            all_pr.append(pr)
+
+        for pr in all_pr:
+            # rez.append(q.get())
+            pr.join()
+            print(f"close process = {pr.name}")
+
+
+if __name__ == "__main__":
+    start_t = time.time()
+    main()
+    print(time.time()-start_t)
